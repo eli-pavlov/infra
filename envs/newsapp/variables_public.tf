88ebd0226c145195fb6d26ed74a9dbe16dfe3029
Eli Pavlov <pavlov.eli@gmail.com>
2025-08-13 12:52:22 +0300

    update repo

--- envs/newsapp/variables_public.tf
+++ envs/newsapp/variables_public.tf
@@ -1,24 +1,66 @@
-variable "ocpus" {
-  type    = number
-  default = 1
+#########################
+# Non-secret variables  #
+#########################
+
+# Display names (used to find-or-create VCN/subnets)
+variable "vcn_display_name" {
+  type        = string
+  description = "Display name of the VCN to reuse or create."
 }
 
-variable "memory_gb" {
-  type    = number
-  default = 6
+variable "subnet_display_name" {
+  type        = string
+  description = "Display name of the PUBLIC subnet to reuse or create."
 }
 
-variable "cloud_init" {
-  type    = string
-  default = ""
+# CIDRs for new deployments (ignored if reusing existing)
+variable "vcn_cidr" {
+  type        = string
+  description = "VCN IPv4 CIDR when creating a new VCN."
+  default     = "10.0.0.0/16"
 }
 
-variable "tags" {
-  type    = map(string)
-  default = {}
+variable "public_subnet_cidr" {
+  type        = string
+  description = "Public subnet CIDR when creating a new public subnet."
+  default     = "10.0.0.0/24"
 }
 
 variable "private_subnet_cidr" {
-  type    = string
-  default = "10.0.1.0/24"
+  type        = string
+  description = "Private subnet CIDR (egress via NAT)."
+  default     = "10.0.1.0/24"
+}
+
+# Node sizing / metadata
+variable "ocpus" {
+  type        = number
+  description = "OCPUs per node."
+  default     = 1
+}
+
+variable "memory_gb" {
+  type        = number
+  description = "Memory (GB) per node."
+  default     = 6
+}
+
+variable "cloud_init" {
+  type        = string
+  description = "Optional cloud-init user data (plain text)."
+  default     = ""
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Freeform tags for created resources."
+  default     = {}
+}
+
+# Allowed CIDRs for public HTTP/HTTPS on node-1
+# Example: '[{"cidr":"84.110.50.0/24"},{"cidr":"87.68.165.0/24"}]'
+variable "ingress_rules_json" {
+  type        = string
+  description = "JSON list of CIDRs allowed to reach ports 80/443 on node-1."
+  default     = "[]"
 }
--- envs/newsapp/variables_secrets.tf
+++ envs/newsapp/variables_secrets.tf
@@ -1,72 +1,58 @@
-# ---- OCI auth ----
+#######################
+# Sensitive variables #
+#######################
+
 variable "tenancy_ocid" {
   type      = string
   sensitive = true
 }
+
 variable "user_ocid" {
   type      = string
   sensitive = true
 }
+
 variable "fingerprint" {
   type      = string
   sensitive = true
 }
+
 variable "private_key_pem" {
   type      = string
   sensitive = true
 }
+
 variable "region" {
   type      = string
   sensitive = true
 }
-variable "ssh_public_key" {
-  type      = string
-  sensitive = true
-}
 
-# ---- placement ----
-variable "availability_domain_number" {
-  type      = number
-  sensitive = true
-}
-variable "fault_domain" {
-  type      = string
-  sensitive = true
-}
-
-# ---- network selection ----
-variable "vcn_display_name" {
-  type      = string
-  sensitive = true
-}
-variable "subnet_display_name" {
-  type      = string
-  sensitive = true
-}
-
-# ---- compartments ----
 variable "compartment_ocid" {
   type      = string
   sensitive = true
 }
+
 variable "network_compartment_ocid" {
   type      = string
   sensitive = true
 }
 
-# ---- image ----
+variable "availability_domain_number" {
+  type      = number
+  sensitive = true
+}
+
+variable "fault_domain" {
+  type      = string
+  sensitive = true
+}
+
 variable "image_ocid" {
   type      = string
   sensitive = true
 }
 
-# ---- public ingress allowlist (CIDRs) as JSON ----
-variable "ingress_rules_json" {
-  type      = string
-  sensitive = true
-}
-
-variable "private_key_path" {
+variable "ssh_public_key" {
   type      = string
   sensitive = true
 }
