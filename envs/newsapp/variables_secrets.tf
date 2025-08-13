28acf818476c4a3665b71ca464547d29970d39b1
Eli Pavlov <pavlov.eli@gmail.com>
2025-08-13 08:05:22 +0300

    update repo

--- envs/newsapp/variables_secrets.tf
+++ envs/newsapp/variables_secrets.tf
@@ -3,27 +3,22 @@ variable "tenancy_ocid" {
   type      = string
   sensitive = true
 }
-
 variable "user_ocid" {
   type      = string
   sensitive = true
 }
-
 variable "fingerprint" {
   type      = string
   sensitive = true
 }
-
 variable "private_key_pem" {
   type      = string
   sensitive = true
 }
-
 variable "region" {
   type      = string
   sensitive = true
 }
-
 variable "ssh_public_key" {
   type      = string
   sensitive = true
@@ -34,41 +29,38 @@ variable "availability_domain_number" {
   type      = number
   sensitive = true
 }
-
 variable "fault_domain" {
   type      = string
   sensitive = true
 }
 
-# ---- network selection (existing VCN + public subnet by name) ----
+# ---- network selection ----
 variable "vcn_display_name" {
   type      = string
   sensitive = true
 }
-
 variable "subnet_display_name" {
   type      = string
   sensitive = true
 }
 
-# compartments
+# ---- compartments ----
 variable "compartment_ocid" {
   type      = string
   sensitive = true
 }
-
 variable "network_compartment_ocid" {
   type      = string
   sensitive = true
 }
 
-# image
+# ---- image ----
 variable "image_ocid" {
   type      = string
   sensitive = true
 }
 
-# public ingress allowlist (CIDRs) as JSON
+# ---- public ingress allowlist (CIDRs) as JSON ----
 variable "ingress_rules_json" {
   type      = string
   sensitive = true
